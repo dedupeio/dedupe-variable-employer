41f098291380fd9481acbbbbb45e1085c7babec0
Forest Gregg <fgregg@uchicago.edu>
2015-12-10 12:40:39 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,5 +12,6 @@ setup(
     packages=['dedupe.variables'],
     install_requires=['graphiqparser',
                       'parseratorvariable >= 0.0.10'],
+    dependency_links=['git+ssh://git@github.com/datamade/graphiqparser.git#egg=graphiqparser'],
     license='The MIT License: http://www.opensource.org/licenses/mit-license.php'
     )
