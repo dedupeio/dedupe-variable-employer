955fa5377f88a9378c44f55ae11b4d2d2cb8bf7b
Forest Gregg <fgregg@uchicago.edu>
2015-12-10 12:27:46 -0600

    initial commit

---
+++ dedupe/variables/employer.py
@@ -0,0 +1,32 @@
+from parseratorvariable import ParseratorType
+import graphiqparser
+import numpy
+
+CORPORATION = (('corporation name',             ('CorporationName', 
+                                                 'ShortForm')),
+               ('corporation org',              ('CorporationNameOrganization',)),
+               ('corporation type',             ('CorporationLegalType',)),
+               ('corporation committee',        ('CorporationCommitteeType',)),
+               ('corporation &Co',              ('CorporationNameAndCompany',)),
+               ('corporation branch',           ('CorporationNameBranchType', 
+                                                 'CorporationNameBranchIdentifier')),
+               ('client corporation name',      ('ProxiedCorporationName', 
+                                                 'ProxiedShortForm')),
+               ('client corporation org',       ('ProxiedCorporationNameOrganization',)),
+               ('client corporation type',      ('ProxiedCorporationLegalType',)),
+               ('client corporation committee', ('ProxiedCorporationCommitteeType',)),
+               ('client corporation &Co',       ('ProxiedCorporationNameAndCompany',)),
+               ('client corporation branch',    ('ProxiedCorporationNameBranchType', 
+                                                 'ProxiedCorporationNameBranchIdentifier')))
+
+
+class EmployerType(ParseratorType) :
+    type = "Employer"
+
+    def __init__(self, definition) :
+        self.components = (('Corporation', self.compareFields, CORPORATION),
+
+        super(EmployerType, self).__init__(definition)
+
+        self.tagger = graphiqparser.tag(field)
+
---
+++ setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup, Extension
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+
+setup(
+    name='dedupe-variable-employer',
+    url='https://github.com/datamade/dedupe-variables-employer',
+    version='0.0.1',
+    description='Graphic Employer variable type for dedupe',
+    packages=['dedupe.variables'],
+    install_requires=['graphiqparser',
+                      'parseratorvariable >= 0.0.10'],
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php'
+    )
